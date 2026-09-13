fedad998ab2fd6199e5799733e94fbf2d5c91cef
MuneebUrRehman005 <muneeburrehman005@gmail.com>
2026-09-13 12:45:52 +0300

    Fix deployment label mismatch

--- src/main/java/com/example/strengthtracker/WorkoutController.java
+++ src/main/java/com/example/strengthtracker/WorkoutController.java
@@ -1,13 +1,14 @@
 package com.example.strengthtracker;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class WorkoutController {
 
     @GetMapping("/workouts")
     public String getWorkouts() {
-        return "Select your workout: 1. Push Day (Chest/Triceps) 2. Pull Day (Back/Biceps) 3. Leg Day";
+        // This tells Spring to load "workouts.html" from your templates folder
+        return "workouts"; 
     }
 }
